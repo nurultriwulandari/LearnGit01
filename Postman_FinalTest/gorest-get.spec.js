f4fee337e3d150ddb9883a81a54ce90f46c0b1dd
Nurul Triwulandari <121376198+nurultriwulandari@users.noreply.github.com>
2023-03-28 21:35:55 +0700

    Update gorest-get.spec.js

--- Postman_FinalTest/gorest-get.spec.js
+++ Postman_FinalTest/gorest-get.spec.js
@@ -2,7 +2,7 @@ describe('Get User List', () => {
     it('Verify the list users will displayed', () => {
         cy.request({
             method: 'GET',
-            url: 'https://gorest.co.in/public/v2/users/261'
+            url: 'https://gorest.co.in/public/v2/users/1084'
         }).as('users')
         cy.get('@users').its('status').should('equal', 200)
     });
