024019963b90fc2167d5765fead9ca2bcae54aa7
Nurul Triwulandari <121376198+nurultriwulandari@users.noreply.github.com>
2023-03-28 23:21:27 +0700

    Delete gorest-get.spec.js

--- Postman_FinalTest/gorest-get.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Get User List', () => {
-    it('Verify the list users will displayed', () => {
-        cy.request({
-            method: 'GET',
-            url: 'https://gorest.co.in/public/v2/users/1084'
-        }).as('users')
-        cy.get('@users').its('status').should('equal', 200)
-    });
-});
